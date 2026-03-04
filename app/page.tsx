26224e62ccff4f5212a827aff1dda48e6fbf4369
carlatvg <carlatatianavalderramagarzon@gmail.com>
2026-03-04 16:26:39 -0500

    chore: initialize next.js project with default configuration

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <title>Astro Routine</title>
+      <h1>Rutina basada en tu carta natal</h1>
+      </main>
+  );
+}
--- index.js
+++
@@ -1,1 +0,0 @@
-console.log("astro routine app");
